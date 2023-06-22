ea8a784589c3773c7e8508b41d2cc987b5d40a73
Таисия Бирюкова <biriuckova.taissa@yandex.ru>
2023-06-22 17:24:47 +0300

    Init prog

---
+++ Book.h
@@ -0,0 +1,14 @@
+//
+// Created by 79023 on 22.06.2023.
+//
+
+#ifndef COOKERYBOOK_BOOK_H
+#define COOKERYBOOK_BOOK_H
+
+
+class Book {
+
+};
+
+
+#endif //COOKERYBOOK_BOOK_H
---
+++ Recipe.h
@@ -0,0 +1,14 @@
+//
+// Created by 79023 on 22.06.2023.
+//
+
+#ifndef COOKERYBOOK_RECIPE_H
+#define COOKERYBOOK_RECIPE_H
+
+
+class Recipe {
+
+};
+
+
+#endif //COOKERYBOOK_RECIPE_H
