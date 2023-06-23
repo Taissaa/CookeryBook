f2047dc1d99276dff251f4487b78cd14441ceeb8
Taissaa <biriuckova.taissa@yandex.ru>
2023-06-23 18:36:12 +0300

    Changes in Recipe.h

--- Recipe.h
+++ Recipe.h
@@ -1,9 +1,13 @@
 #ifndef COOKERYBOOK_RECIPE_H
 #define COOKERYBOOK_RECIPE_H
 
+#include "Ingredients .h"
+#include <vector>
 
 class Recipe {
-
+    std::string name;
+    std::vector<Ingredient>ingredients;
+    std::string making; 
 };
 
 
