745b3117f8e34fbe492266c1b3f43a76aab6369e
Taissaa <biriuckova.taissa@yandex.ru>
2023-06-24 00:01:43 +0300

    Add testing

--- main.cpp
+++ main.cpp
@@ -6,11 +6,17 @@ int main() {
     Ingredient ingredient3("Toast", 300);
     Ingredient ingredient4("Egg", 3);
 
+    Ingredient ingredient5("Flour", 400);
+    Ingredient ingredient6("Yeast", 20);
+    Ingredient ingredient7("Salt", 7);
+
     Recipe recipe1("french toast", {ingredient1, ingredient2, ingredient3, ingredient4}, "1. Miks all\n2. Make fry");
+    Recipe recipe2("bread", {ingredient1, ingredient5, ingredient6, ingredient7}, "Miks all\n2.Pour into the modl\n3.Bake");
 
     Book cookbook;
     cookbook.addRecipe(recipe1);
-    
+    cookbook.addRecipe(recipe2);
+
     cookbook.displayCookbook();
 
 
